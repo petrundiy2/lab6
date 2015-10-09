1c28d1c80a9d7ed29878c7afbebef0fe80f072e3
petrundiy2 <petrundiy1@gmail.com>
2015-10-10 00:50:15 +0300

    Update A.py

--- A.py
+++ A.py
@@ -1,13 +1,17 @@
-N=int(input())
+a=open('input.txt','r')
+b=open('output.txt','w')
+s=a.readline()
+f=a.readline()
 A=[]
-p=0
-for x in range(N):
-    A.append(int(input()))
-for x in range(N):
+h=''
+for x in range(int(s)*2):
+    if f[x]!=' ':
+        A.append(f[x])
+for x in range(len(A)):
     k=0
-    for y in range(N):
+    for y in range(len(A)):
         if A[x]==A[y] and x!=y:
             k+=1
     if k==1:
-        p=A[x]
-print(p)
+        h=A[x]
+print(eval(h))
