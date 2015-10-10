24d6008e846aee2fcf12d9489c2ffe68bbe0022f
petrundiy2 <petrundiy1@gmail.com>
2015-10-11 02:20:46 +0300

    Update A.py

--- A.py
+++ A.py
@@ -1,21 +1,14 @@
-a=open('input.txt','r')
-b=open('output.txt','w')
-s=a.readline()
-f=a.readline()
-g=f+' '
-A=[]
-h=''
-y=0
-for x in range(len(g)):
-    if g[x]==' ':
-        A.append(eval(g[y:x]))
-        y=x
-for x in range(len(A)):
-    k=0
-    for y in range(len(A)):
-        if A[x]==A[y] and x!=y:
-            k+=1
-    if k==1:
-        h=A[x]
-if h!='':
-    print(h,file=b)
+__author__ = 'Petr'
+with open('input.txt', 'r') as inp:
+    with open('output.txt', 'w') as out:
+        N = int(inp.readline())
+        A = inp.readline().split()
+        B=[]
+        x=0
+        while x<=N:
+            for y in range(N):
+                if x!=y and A[x]==A[y]:
+                    print(A[x],file=out)
+                    x=N
+                    break
+            x+=1
