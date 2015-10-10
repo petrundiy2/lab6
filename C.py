577e0f65717d0b27538e1fcf1b9efd968b3b7b72
petrundiy2 <petrundiy1@gmail.com>
2015-10-11 00:24:17 +0300

    Update C.py

--- C.py
+++ C.py
@@ -1,12 +1,14 @@
-N=int(input())
-A=[]
-for x in range(N):
-    A.append(int(input()))
-k=N
-for x in range (N):
-    for y in range(N):
-        if (A[x])==-(A[y]) and A[x]<0 and x!=y:
-            if abs(x-y)<k:
-                k=abs(x-y)
-if k!=N:
-    print (k)
+with open('input.txt', 'r') as inp:
+    with open('output.txt', 'w') as out:
+        N = int(inp.readline())
+        A = list(map(int, inp.readline().split()))
+        k=N+1
+        for x in range (N):
+            for y in range(N):
+                if (A[x])==-(A[y]) and A[x]<0 and x<y:
+                    if abs(x-y)<k:
+                        k=abs(x-y)
+        if k!=N+1:
+            print (k,file=out)
+        if k==N+1:
+            print(0,file=out)
